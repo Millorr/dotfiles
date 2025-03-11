5d149bda67c73ce01f942e9432d1701843dbaec7
Guhl, Marc <marcguhl@gmail.com>
2025-03-11 13:01:46 +0100

    Followup

--- nvim/lua/marc/lazy.lua
+++ nvim/lua/marc/lazy.lua
@@ -11,4 +11,13 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("marc.plugins")
+require("lazy").setup(
+  {
+    rocks = {
+      enabled = false
+    }
+  },
+  {
+    import = "marc.plugins"
+  }
+})
